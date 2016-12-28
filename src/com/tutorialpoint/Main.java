36ed8c5969a78bc58b0ec1bfa7c745c2eeb4402d
Mark Rothermal <rothermal2@comcast.net>
2016-12-27 22:07:50 -0600

    init and destroy

--- src/com/tutorialpoint/Main.java
+++ src/com/tutorialpoint/Main.java
@@ -1,35 +1,40 @@
 package com.tutorialpoint;
 
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 	public static void main (String [] args){
 		
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
 //	this is just the basic hello world.	
 //		HelloWorld obj = (HelloWorld) context.getBean("helloWorld"); 
 //		obj.getMessage();
 
 // this is hello world with singleton scope - note code is the same for prototype
-// for singleton, this will create the message, i am object a for both entries
-// for prototype it will print the declared message for a and the default message for obj b
+// for singleton - this will create the message, i am object a for both entries
+// for prototype it will print the i am object a for a and the default message 
+// set in the bean for obj b.
 // there are also request, session, global session scopes, 
 // request scope - scopes a bean to an http request
 // session scope - scopes  bean to an http session
 // global session - scopes a bean to a global http session		
 		
-		HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+//		HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+//		
+//		objA.setMessage("I am Object A");
+//		objA.getMessage();
+//		
+//		HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+//		objB.getMessage();
 		
-		objA.setMessage("I am Object A");
-		objA.getMessage();
 		
-		HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
-		objB.getMessage();
-		
-		
-// hello world prototype scope
+// hello world spring bean life cycle
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+		context.registerShutdownHook();
 		
 	} 
 }
